7e8ce5e44cd9141365069b3a2960d54d9344456c
Danapsta <bayates826@gmail.com>
2023-09-08 15:36:37 -0500

    modified:   Download.bat

--- Download.bat
+++ Download.bat
@@ -4,67 +4,62 @@
 set psScript=%~dpn0.ps1
 
 :: create the PS script
-(
-echo $[char]34
-Add-Type -AssemblyName System.Windows.Forms
+echo Add-Type -AssemblyName System.Windows.Forms > %psScript%
 
-$form = New-Object System.Windows.Forms.Form 
-$form.Text = "Script Interface"
-$form.Size = New-Object System.Drawing.Size(300,200) 
-$form.StartPosition = "CenterScreen"
+echo $form = New-Object System.Windows.Forms.Form >> %psScript%
+echo $form.Text = "Script Interface" >> %psScript%
+echo $form.Size = New-Object System.Drawing.Size(300,200) >> %psScript% 
+echo $form.StartPosition = "CenterScreen" >> %psScript%
 
-$DownloadDeployButton = New-Object System.Windows.Forms.Button
-$DownloadDeployButton.Location = New-Object System.Drawing.Point(50,30)
-$DownloadDeployButton.Size = New-Object System.Drawing.Size(200,30)
-$DownloadDeployButton.Text = "1. Download Deployment Script"
-$DownloadDeployButton.Add_Click({
-    DownloadScript "https://github.com/danapsta/Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main"
-})
-$form.Controls.Add($DownloadDeployButton)
+echo $DownloadDeployButton = New-Object System.Windows.Forms.Button >> %psScript%
+echo $DownloadDeployButton.Location = New-Object System.Drawing.Point(50,30) >> %psScript%
+echo $DownloadDeployButton.Size = New-Object System.Drawing.Size(200,30) >> %psScript%
+echo $DownloadDeployButton.Text = "1. Download Deployment Script" >> %psScript%
+echo $DownloadDeployButton.Add_Click({  >> %psScript%
+echo    DownloadScript "https://github.com/danapsta/Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main"  >> %psScript%
+echo }) >> %psScript%
+echo $form.Controls.Add($DownloadDeployButton) >> %psScript%
 
-$DownloadAndRunDeployButton = New-Object System.Windows.Forms.Button
-$DownloadAndRunDeployButton.Location = New-Object System.Drawing.Point(50,70)
-$DownloadAndRunDeployButton.Size = New-Object System.Drawing.Size(200,30)
-$DownloadAndRunDeployButton.Text = "2. Download (and run) Deployment Script"
-$DownloadAndRunDeployButton.Add_Click({
-    DownloadScript "https://github.com/danapsta/Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main"
-    Start-Process "$env:USERPROFILE\Desktop\Deploy.bat"
-})
-$form.Controls.Add($DownloadAndRunDeployButton)
+echo $DownloadAndRunDeployButton = New-Object System.Windows.Forms.Button >> %psScript%
+echo $DownloadAndRunDeployButton.Location = New-Object System.Drawing.Point(50,70) >> %psScript%
+echo $DownloadAndRunDeployButton.Size = New-Object System.Drawing.Size(200,30) >> %psScript%
+echo $DownloadAndRunDeployButton.Text = "2. Download (and run) Deployment Script" >> %psScript%
+echo $DownloadAndRunDeployButton.Add_Click({ >> %psScript%
+echo     DownloadScript "https://github.com/danapsta/Deploy/archive/refs/heads/main.zip" "Deploy.zip" "Deploy-main" >> %psScript%
+echo     Start-Process "$env:USERPROFILE\Desktop\Deploy.bat" >> %psScript%
+echo }) >> %psScript%
+echo $form.Controls.Add($DownloadAndRunDeployButton) >> %psScript%
 
-$DownloadMigrationButton = New-Object System.Windows.Forms.Button
-$DownloadMigrationButton.Location = New-Object System.Drawing.Point(50,110)
-$DownloadMigrationButton.Size = New-Object System.Drawing.Size(200,30)
-$DownloadMigrationButton.Text = "3. Download Migration Script"
-$DownloadMigrationButton.Add_Click({
-    DownloadScript "https://github.com/danapsta/Migration/archive/refs/heads/main.zip" "Migration.zip" "Migration-main"
-})
-$form.Controls.Add($DownloadMigrationButton)
+echo $DownloadMigrationButton = New-Object System.Windows.Forms.Button >> %psScript%
+echo $DownloadMigrationButton.Location = New-Object System.Drawing.Point(50,110) >> %psScript%
+echo $DownloadMigrationButton.Size = New-Object System.Drawing.Size(200,30) >> %psScript%
+echo $DownloadMigrationButton.Text = "3. Download Migration Script" >> %psScript%
+echo $DownloadMigrationButton.Add_Click({ >> %psScript%
+echo     DownloadScript "https://github.com/danapsta/Migration/archive/refs/heads/main.zip" "Migration.zip" "Migration-main" >> %psScript%
+echo }) >> %psScript%
+echo $form.Controls.Add($DownloadMigrationButton) >> %psScript%
 
-$DownloadAndRunMigrationButton = New-Object System.Windows.Forms.Button
-$DownloadAndRunMigrationButton.Location = New-Object System.Drawing.Point(50,150)
-$DownloadAndRunMigrationButton.Size = New-Object System.Drawing.Size(200,30)
-$DownloadAndRunMigrationButton.Text = "4. Download and Run Migration Script"
-$DownloadAndRunMigrationButton.Add_Click({
-    DownloadScript "https://github.com/danapsta/Migration/archive/refs/heads/main.zip" "Migration.zip" "Migration-main"
-    Start-Process "$env:USERPROFILE\Desktop\Download-export.bat"
-})
-$form.Controls.Add($DownloadAndRunMigrationButton)
+echo $DownloadAndRunMigrationButton = New-Object System.Windows.Forms.Button >> %psScript%   
+echo $DownloadAndRunMigrationButton.Location = New-Object System.Drawing.Point(50,150) >> %psScript%
+echo $DownloadAndRunMigrationButton.Size = New-Object System.Drawing.Size(200,30) >> %psScript%
+echo $DownloadAndRunMigrationButton.Text = "4. Download and Run Migration Script" >> %psScript%
+echo $DownloadAndRunMigrationButton.Add_Click({ >> %psScript%
+echo     DownloadScript "https://github.com/danapsta/Migration/archive/refs/heads/main.zip" "Migration.zip" "Migration-main" >> %psScript%
+echo     Start-Process "$env:USERPROFILE\Desktop\Download-export.bat" >> %psScript%
+echo }) >> %psScript%
+$form.Controls.Add($DownloadAndRunMigrationButton) >> %psScript%
 
-function DownloadScript($url, $zipName, $folderName){
-    $desktop = [Environment]::GetFolderPath("Desktop")
-    Invoke-WebRequest -Uri $url -OutFile "$desktop\$zipName"
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$desktop\$zipName", "$desktop")
-    Remove-Item "$desktop\$zipName"
-    Get-ChildItem -Path "$desktop\$folderName" | Move-Item -Destination $desktop
-    Remove-Item "$desktop\$folderName" -recurse -force
-}
+echo function DownloadScript($url, $zipName, $folderName){ >> %psScript%
+echo     $desktop = [Environment]::GetFolderPath("Desktop") >> %psScript%
+echo     Invoke-WebRequest -Uri $url -OutFile "$desktop\$zipName" >> %psScript%
+echo     Add-Type -AssemblyName System.IO.Compression.FileSystem >> %psScript%
+echo     [System.IO.Compression.ZipFile]::ExtractToDirectory("$desktop\$zipName", "$desktop") >> %psScript%
+echo     Remove-Item "$desktop\$zipName" >> %psScript%
+echo     Get-ChildItem -Path "$desktop\$folderName" | Move-Item -Destination $desktop >> %psScript%
+echo     Remove-Item "$desktop\$folderName" -recurse -force >> %psScript%
+echo } >> %psScript%
 
-$form.ShowDialog()
-
-echo $[char]34
-) > "%psScript%"
+echo $form.ShowDialog() >> %psScript%
 
 :: run the PS script
 powershell -ExecutionPolicy Bypass -File "%psScript%"
